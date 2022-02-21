8ef63a4d6bdead92b538924f6225c82eb62a3881
cannelle hennuyer <cannelle.hennuyer@gmail.com>
2022-02-21 15:44:04 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :powers, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  get "bookings", to: "pages#bookings"
+  get "past-bookings", to: "pages#past-bookings"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/powers_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PowersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get powers_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get powers_show_url
+    assert_response :success
+  end
+end
