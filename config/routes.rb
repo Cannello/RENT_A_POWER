02f6e878e1eef888c2e98cf610f66f03b280e6e8
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-22 12:41:38 +0000

    started working on bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
   devise_for :users
   root to: 'pages#home'
   resources :powers, only: %I[index show] do
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   end
 
   get "bookings", to: "pages#bookings"
-  get "past-bookings", to: "pages#past-bookings"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
