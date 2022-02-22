9ef5ddf1938a15cd2db17f01577d35b014e02379
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-22 15:33:49 +0000

    people can create and see their own bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,25 @@
 class BookingsController < ApplicationController
-  def create
+  def new
+    @booking = Booking.new
+    @power = Power.find(params[:power_id])
+  end
 
+  def create
+    @booking = Booking.new(set_booking)
+    @power = Power.find(params[:power_id])
+    @booking.power = @power
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "bookings", to: "pages#bookings"
   resources :powers, only: %i[index show] do
     resources :bookings, only: %i[new create]
   end
-
-  get "bookings", to: "pages#bookings"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
