e800c4813da804c04ac02e94b978a3fe77861f26
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-24 11:03:02 +0000

    Added validations to users and powers, changed seeds file to adjust description

--- app/models/power.rb
+++ app/models/power.rb
@@ -2,4 +2,6 @@ class Power < ApplicationRecord
   has_one_attached :photo
   has_many :bookings
   has_many :users, through: :bookings
+  validates :name, :description, uniqueness: true
+  validates :description, length: { maximum: 475 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :powers, through: :bookings
+  validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
