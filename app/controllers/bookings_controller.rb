a9b7a9b57c795f5d444a59bda2e531c5911f5a54
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-24 14:10:46 +0000

    added more validations to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,15 @@
 class BookingsController < ApplicationController
+  before_action :set_power, only: %i[new create edit]
+  before_action :set_booking, only: %i[edit]
   def new
     @booking = Booking.new
-    @power = Power.find(params[:power_id])
+    authorize @booking
     authorize @power
   end
 
   def create
-    @booking = Booking.new(set_booking)
-    @power = Power.find(params[:power_id])
+    @booking = Booking.new(booking_params)
+    authorize @booking
     authorize @power
     @booking.power = @power
     @booking.user = current_user
@@ -19,9 +21,23 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+
+  end
+
   private
 
+  def set_power
+    @power = Power.find(params[:power_id])
+  end
+
   def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
 end
